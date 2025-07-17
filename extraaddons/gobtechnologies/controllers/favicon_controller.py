0db4893bd8bb505a76b9351a473fa6663e0a271a
Kenneth02658 <kenneth@senseitechnology.com>
2025-07-17 17:35:07 +0000

    Favicon added

--- extraaddons/gobtechnologies/controllers/favicon_controller.py
+++
@@ -1,8 +0,0 @@
-from odoo import http
-from odoo.http import request
-import os
-
-class FaviconController(http.Controller):
-    @http.route(['/web/favicon.ico', '/favicon.ico'], type='http', auth="public", website=True)
-    def favicon(self, **kw):
-        return request.env['ir.http']._content_manifest('gobtechnologies', 'static/description/splitpay.png')
