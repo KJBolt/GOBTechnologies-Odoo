e30ab0b487971ce0b10efbb48891e2062facaa80
Kenneth02658 <kenneth@senseitechnology.com>
2025-09-23 16:50:53 +0000

    Minor changes

--- extraaddons/gobtechnologies/models/__init__.py
+++ extraaddons/gobtechnologies/models/__init__.py
@@ -2,4 +2,4 @@ from . import customer_statement_report
 from . import hubtel_webhook
 from . import repayment_penalty
 from . import res_config_settings
-from . import payment_notifications
+from . import invoice_webhook
