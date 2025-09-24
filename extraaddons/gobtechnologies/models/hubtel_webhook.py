ccd1818502304ca822bb61575e2707faa8014369
Kenneth02658 <kenneth@senseitechnology.com>
2025-09-24 10:57:45 +0000

    Hubtel webhook changes

--- extraaddons/gobtechnologies/models/hubtel_webhook.py
+++ extraaddons/gobtechnologies/models/hubtel_webhook.py
@@ -6,7 +6,7 @@ _logger = logging.getLogger(__name__)
 class HubtelWebhook(models.Model):
     _name = 'hubtel.webhook'
     _description = 'Hubtel Webhook'
-    _auto = False
+    _auto = True
     
     # Data Fields
     message = fields.Char(string='Message', readonly=True)
